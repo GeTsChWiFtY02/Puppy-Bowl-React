05726fc92a9bfc1eac024f41ace72f89336a1557
GeTsChWiFtY02 <141801363+GeTsChWiFtY02@users.noreply.github.com>
2023-11-23 16:15:53 -0800

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+import Navbar from './components/NavBar'
+import MainRoute from './components/MainRoute'
+import NewPlayerForm from './components/NewPlayerForm'
+
+function App() {
+
+
+  return (
+    <div>
+      <Navbar />
+      <NewPlayerForm />
+      <MainRoute />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+//import Link from react-router-dom
+import { Link } from "react-router-dom";
+
+
+const Navbar = () => {
+    return(
+        <div className="navbar">
+            <Link to="/" className="home">Home</Link>
+            <h3>Puppy Bowl</h3>
+        </div>
+    )
+}
+
+export default Navbar;
