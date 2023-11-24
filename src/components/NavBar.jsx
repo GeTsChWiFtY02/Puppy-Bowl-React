308ebe774241938dac0b4a38560cde8605a68702
GeTsChWiFtY02 <141801363+GeTsChWiFtY02@users.noreply.github.com>
2023-11-23 21:56:56 -0800

    Update NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -7,8 +7,9 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
     return(
         <div className="navbar">
-            <Link to="/" className="home">Home</Link>
             <h3>Puppy Bowl</h3>
+            <Link to="/" className="home">Home</Link>
+            
         </div>
     )
 }
